c60fd8042af7e8a5ef2ab9105f3bcd39a6a025a6
rajukumar2152 <rajukumar2152chd@gmail.com>
2021-05-12 03:43:25 +0530

    sqlite3 is commited

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -88,12 +88,12 @@ WSGI_APPLICATION = 'the_weather.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': dj_database_url.config()
 }
