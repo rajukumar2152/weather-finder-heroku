29a648a278bb7e9a12da7da135090580b59526ed
rajukumar2152 <rajukumar2152chd@gmail.com>
2021-05-12 03:38:30 +0530

    changes in setting

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -93,6 +94,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # import dj_database_url
 
@@ -137,3 +141,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+django_heroku.settings(locals())
+
